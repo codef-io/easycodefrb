c57db16eab272b4ba838cd8e87edd889b328c2a6
dc7303 <dc7303@gmail.com>
2020-05-25 14:44:25 +0900

    request_api 요청 메소드 구현 및 테스트
    - 다른 API에서 request_product 메소드로 구현되어 있으나, execute 실행시
    self를 넘겨주게 되면 connector.rb에 동일한 메소드 호출 시
    self.request_product를 호출하게 되는 현상이 발생. 그래서 request_api로
    변경

--- lib/easycodef/easycodef.rb
+++ lib/easycodef/easycodef.rb
@@ -1,3 +1,6 @@
+require_relative './message'
+require_relative './connector'
+
 module EasyCodef
   class RequestInfo
     def initialize(domain, client_id, client_secret)
@@ -87,5 +90,50 @@ module EasyCodef
         )
       end
     end
+
+    # API 요청
+    def request_api(product_path, service_type, param)
+      valid_flag = true
+
+      valid_flag = check_client_info(service_type)
+      # 클라이언트 정보 검사
+      if !valid_flag
+        res = new_response_message(Message::EMPTY_CLIENT_INFO)
+        return res.to_json()
+      end
+
+      # 추가인증 키워드 체크
+      valid_flag = check_two_way_keyword(param)
+      if !valid_flag
+        res = new_response_message(Message::INVALID_2WAY_KEYWORD)
+        return res.to_json()
+      end
+
+      # 리퀘스트 정보 조회
+      req_info = get_req_info_by_service_type(service_type)
+
+      # 요청
+      res = execute(product_path, param, self, req_info)
+      return res.to_json()
+    end
+
+    private
+
+    # 클라이언트 정보 검사
+    def check_client_info(service_type)
+      return case service_type
+      when TYPE_PRODUCT
+        @client_id.strip != '' && client_secret.strip != ''
+      when TYPE_DEMO
+        @demo_client_id.strip != '' && @demo_client_secret.strip != ''
+      else
+        SANDBOX_CLIENT_ID.strip != '' && SANDBOX_CLIENT_SECRET.strip != ''
+      end
+    end
   end
+end
+
+# 2Way 키워드 존재 여부 검사
+def check_two_way_keyword(param)
+  return param['is2Way'] == nil && param['twoWayInfo'] == nil
 end
--- test/test_easycodef.rb
+++ test/test_easycodef.rb
@@ -1,5 +1,8 @@
 require 'minitest/autorun'
+require 'json'
 require 'easycodef'
+require 'easycodef/message'
+require_relative './test_connector'
 
 class EasyCodefTest < Minitest::Test
 
@@ -22,5 +25,74 @@ class EasyCodefTest < Minitest::Test
     assert req_info.domain == EasyCodef::SANDBOX_DOMAIN
     assert req_info.client_id == EasyCodef::SANDBOX_CLIENT_ID
     assert req_info.client_secret == EasyCodef::SANDBOX_CLIENT_SECRET
+  end
+
+  # 클라이언트 정보 셋팅 검사 테스트
+  def test_check_client_info()
+    codef = EasyCodef::Codef.new()
+
+    flag = codef.send(:check_client_info, EasyCodef::TYPE_SANDBOX)
+    assert flag == true
+    flag = codef.send(:check_client_info, EasyCodef::TYPE_PRODUCT)
+    assert flag == false
+    flag = codef.send(:check_client_info, EasyCodef::TYPE_DEMO)
+    assert flag == false
+
+    codef.set_client_info('real', 'secret')
+    codef.set_client_info_for_demo('demo', 'demo_secret')
+    flag = codef.send(:check_client_info, EasyCodef::TYPE_PRODUCT)
+    assert flag == true
+    flag = codef.send(:check_client_info, EasyCodef::TYPE_DEMO)
+    assert flag == true
+  end
+
+  # request_product 테스트
+  def test_request_api()
+    # 클라이언트 정보 체크
+    codef = EasyCodef::Codef.new()
+    param = create_param_for_create_connected_id()
+    res = codef.request_api(
+      EasyCodef::PATH_CREATE_ACCOUNT,
+      EasyCodef::TYPE_PRODUCT,
+      param
+    )
+    res = JSON.parse(res)
+    assert res['result']['code'] == Message::EMPTY_CLIENT_INFO.code
+
+    # 2way 키워드 존재여부 검사
+    param_2way = {
+      'is2Way'=>true
+    }
+    res = codef.request_api(
+      EasyCodef::PATH_CREATE_ACCOUNT,
+      EasyCodef::TYPE_SANDBOX,
+      param_2way
+    )
+    res = JSON.parse(res)
+    assert res['result']['code'] == Message::INVALID_2WAY_KEYWORD.code
+
+    # 결과 테스트
+    res = codef.request_api(
+      EasyCodef::PATH_CREATE_ACCOUNT,
+      EasyCodef::TYPE_SANDBOX,
+      param
+    )
+    res = JSON.parse(res)
+    assert res['data']['connectedId'] != nil
+  end
+
+  # 2way 키워드 체크 테스트
+  def test_check_two_way_keyword()
+    param = {}
+    flage = check_two_way_keyword(param)
+    assert flage == true
+
+    param['is2Way'] = true
+    flage = check_two_way_keyword(param)
+    assert flage == false
+
+    param['twoWayInfo'] = {}
+    flage = check_two_way_keyword(param)
+    assert flage == false
   end
 end
